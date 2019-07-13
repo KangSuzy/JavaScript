2caca9faff93ed22559a14a4dcf73e7b123508d3
KangSuzy <astnwl321@gmail.com>
2019-07-13 17:04:17 +0900

    string 실습

---
+++ lecture13.js
@@ -0,0 +1,15 @@
+var str = "Hello";
+console.log(str.length);
+console.log(str["length"]);
+
+var str2 = " World";
+console.log(str.concat(str2));
+
+var str3 = str.concat(str2);
+console.log(str3.concat("!"));
+
+console.log("Hello".concat(" suzy"));
+console.log("Hello".length);
+
+console.log(str+str2);
+console.log("Pi is "+3.14);
