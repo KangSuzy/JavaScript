c264fde06fc44cbb56a6f02ee8904e448b989cd5
KangSuzy <astnwl321@gmail.com>
2019-07-13 16:48:30 +0900

    연산자 우선순위 이해

---
+++ lecture12.js
@@ -0,0 +1,7 @@
+2 * 3 > 4 + 5 && 6 / 3 == 2 || !false
+
+height >= 180 && gender == "male" || height >= 165 && gender == "female"
+
+(height >= 180 && gender == "male") || (height >= 165 && gender == "female")
+
+// 우선순위를 명시하기위해 괄호 사용 -> 연산자의 우선순위 실수방지, 코드의 가독성 향상
