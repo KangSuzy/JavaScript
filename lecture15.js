4fd42b1965c48652fd48de8aed53647bdc42127b
KangSuzy <astnwl321@gmail.com>
2019-07-15 12:27:56 +0900

    배열의 선언과 접근

---
+++ lecture15.js
@@ -0,0 +1,10 @@
+var arr=[]; //빈배열 생성
+var arr2 = [1,2,3,4,5];
+console.log(arr2);
+console.log(arr.length); //빈배열 길이 0
+console.log(arr2.length);
+
+
+var mixed_arr = [1, true, 3.14, "String", {name:"object"}, [1,2,3]];
+console.log(mixed_arr);
+console.log(mixed_arr.length);
